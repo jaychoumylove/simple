96708427ceed873180324d42114c7259dd650712
caixian <qq52577517@163.com>
2019-04-01 21:54:11 +0800

    usestate in handleObject

--- src/component/R.tsx
+++ src/component/R.tsx
@@ -7,13 +7,27 @@ interface OwnProps extends RouteComponentProps {
 
 const Counter:FunctionComponent<OwnProps> = (initState) => {
     const [clicks, setClicks] = useState(initState.initial);
+    const InitState:{token: String,auth: Boolean} = { token: 'no', auth: false };
+    const [state, handelState] = useState(InitState);
 
-    console.log(initState);
+    function _handelState() {
+        const token = state.auth ? 'yes': 'no';
+
+        handelState(state => {
+            return {
+                token,
+                auth: !state.auth
+            }
+        })
+    }
 
     return <>
         <p>Clicks: {clicks}</p>
         <button onClick={() => setClicks(clicks+1)}>+</button>
         <button onClick={() => setClicks(clicks-1)}>-</button>
+        <p>token: {state.token}</p>
+        <p>auth: {state.auth ? 'true': 'false'}</p>
+        <button onClick={() => _handelState()}>handleState</button>
     </>
 }
 
